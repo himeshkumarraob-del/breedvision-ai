0af33c085d466ffd2fca0084b66b37d906d2024e
Himesh Kumar Rao <himeshkumarraob@gmail.com>
2026-06-06 16:02:00 +0530

    Add index.html entry and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/breedvision-ai/',
+    base: './',
 
     resolve: {
       alias: {
